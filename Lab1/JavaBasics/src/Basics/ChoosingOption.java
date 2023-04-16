73cb1e8c801bb02587b631999ce1ad1995c2671c
Viet Quang <93778662+helloitsurdvq@users.noreply.github.com>
2023-04-16 10:39:05 +0700

    adding demo for the answer of 6.1

--- Lab1/JavaBasics/src/Basics/ChoosingOption.java
+++ Lab1/JavaBasics/src/Basics/ChoosingOption.java
@@ -6,7 +6,16 @@ public class ChoosingOption {
 				(null, "Do you want to change to the first class ticket?");
 		JOptionPane.showMessageDialog(null, "You've chosen: "
 				+ (option==JOptionPane.YES_OPTION? "Yes" : "No"));
-		System.exit(0);
+//		demo for answer 6.1
+//		String[] options = {"I do", "I don't"};
+//		int option = JOptionPane.showOptionDialog(null, 
+//				"Do you want to change to the first class ticket??",
+//				"Title", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, 
+//				options, options[0]);
+//
+//		JOptionPane.showMessageDialog(null, "You've chosen: "
+//				+ (option==JOptionPane.YES_OPTION ? "Yes" : "No"));
+//		System.exit(0);
+		
 	}
 }
-
